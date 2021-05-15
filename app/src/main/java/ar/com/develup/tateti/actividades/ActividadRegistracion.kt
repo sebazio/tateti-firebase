4f0df92baf70d159795fe768084c55d1685b82b6
Fabio Negri <fabionegri1990@gmail.com>
2021-05-14 22:41:47 -0300

    Limpieza de pantalla ActividadRegistracion.kt

--- app/src/main/java/ar/com/develup/tateti/actividades/ActividadRegistracion.kt
+++ app/src/main/java/ar/com/develup/tateti/actividades/ActividadRegistracion.kt
@@ -3,16 +3,9 @@ package ar.com.develup.tateti.actividades
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ar.com.develup.tateti.R
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import kotlinx.android.synthetic.main.actividad_registracion.*
 
-/**
- * Created by maribelmai on 26/3/17.
- */
 class ActividadRegistracion : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,29 +14,45 @@ class ActividadRegistracion : AppCompatActivity() {
         registrar.setOnClickListener { registrarse() }
     }
 
-    private val registracionCompletaListener: OnCompleteListener<AuthResult?> = OnCompleteListener { task ->
-        if (task.isSuccessful) {
-            Snackbar.make(rootView, "Registro exitoso", Snackbar.LENGTH_SHORT).show()
-            FirebaseAuth.getInstance().currentUser.sendEmailVerification()
-        } else if (task.exception is FirebaseAuthUserCollisionException) {
-            Snackbar.make(rootView, "El usuario ya existe", Snackbar.LENGTH_SHORT).show()
-        } else {
-            Snackbar.make(rootView, "El registro fallo: " + task.exception, Snackbar.LENGTH_LONG).show()
-        }
-    }
-
-
     fun registrarse() {
         val passwordIngresada = password.text.toString()
         val confirmarPasswordIngresada = confirmarPassword.text.toString()
-        if (email.text.toString().isEmpty()) {
+        val email = email.text.toString()
+
+        if (email.isEmpty()) {
+            // Si no completo el email, muestro mensaje de error
             Snackbar.make(rootView, "Email requerido", Snackbar.LENGTH_SHORT).show()
         } else if (passwordIngresada == confirmarPasswordIngresada) {
-            FirebaseAuth.getInstance()
-                    .createUserWithEmailAndPassword(email.text.toString(), passwordIngresada)
-                    .addOnCompleteListener(this, registracionCompletaListener)
+            // Si completo el email y las contraseñas coinciden, registramos el usuario en Firebase
+            registrarUsuarioEnFirebase(email, passwordIngresada)
         } else {
+            // No coinciden las contraseñas, mostramos mensaje de error
             Snackbar.make(rootView, "Las contraseñas no coinciden", Snackbar.LENGTH_SHORT).show()
         }
+    }
+
+    private fun registrarUsuarioEnFirebase(email: String, passwordIngresada: String) {
+        // TODO-05-AUTHENTICATION
+        // Crear el usuario con el email y passwordIngresada
+        // Ademas, registrar en CompleteListener el listener registracionCompletaListener definido mas abajo
+    }
+
+//    private val registracionCompletaListener: OnCompleteListener<AuthResult?> = OnCompleteListener { task ->
+//        if (task.isSuccessful) {
+//            // Si se registro OK, muestro mensaje y envio mail de verificacion
+//            Snackbar.make(rootView, "Registro exitoso", Snackbar.LENGTH_SHORT).show()
+//            enviarEmailDeVerificacion()
+//        } else if (task.exception is FirebaseAuthUserCollisionException) {
+//            // Si el usuario ya existe, mostramos error
+//            Snackbar.make(rootView, "El usuario ya existe", Snackbar.LENGTH_SHORT).show()
+//        } else {
+//            // Por cualquier otro error, mostramos un mensaje de error
+//            Snackbar.make(rootView, "El registro fallo: " + task.exception, Snackbar.LENGTH_LONG).show()
+//        }
+//    }
+
+    private fun enviarEmailDeVerificacion() {
+        // TODO-05-AUTHENTICATION
+        // Enviar mail de verificacion al usuario currentUser
     }
 }
