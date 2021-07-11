3425e67960732aeb219005aac0bac364a4bc2ebc
szuviria <szuviria@gmail.com>
2021-07-11 01:59:13 -0300

    Agregado TODO 1,2,3,4,5,6,7 TERMINADO

--- app/src/main/java/ar/com/develup/tateti/actividades/ActividadPartidas.kt
+++ app/src/main/java/ar/com/develup/tateti/actividades/ActividadPartidas.kt
@@ -2,10 +2,18 @@ package ar.com.develup.tateti.actividades
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import ar.com.develup.tateti.R
 import ar.com.develup.tateti.adaptadores.AdaptadorPartidas
+import ar.com.develup.tateti.modelo.Constantes
+import ar.com.develup.tateti.modelo.Partida
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.actividad_partidas.*
 
 class ActividadPartidas : AppCompatActivity() {
@@ -25,38 +33,26 @@ class ActividadPartidas : AppCompatActivity() {
         nuevaPartida.setOnClickListener { nuevaPartida() }
     }
 
-    override fun onResume() {
-        super.onResume()
-        // TODO-06-DATABASE
-        // Obtener una referencia a la base de datos, suscribirse a los cambios en Constantes.TABLA_PARTIDAS
-        // y agregar como ChildEventListener el listenerTablaPartidas definido mas abajo
-    }
-
-    fun nuevaPartida() {
-        val intent = Intent(this, ActividadPartida::class.java)
-        startActivity(intent)
-    }
-/*
     private val listenerTablaPartidas: ChildEventListener = object : ChildEventListener {
 
         override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
             Log.i(TAG, "onChildAdded: $dataSnapshot")
-            val partida = ??? // Obtener el valor del dataSnapshot
-            partida.id = ??? // Asignar el valor del campo "key" del dataSnapshot
+            val partida = dataSnapshot.getValue(Partida::class.java) // Obtener el valor del dataSnapshot
+            partida!!.id = dataSnapshot.key // Asignar el valor del campo "key" del dataSnapshot
             adaptadorPartidas.agregarPartida(partida)
         }
 
         override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {
             Log.i(TAG, "onChildChanged: $s")
-            val partida = ??? // Obtener el valor del dataSnapshot
-            partida.id = ??? // Asignar el valor del campo "key" del dataSnapshot
+            val partida = dataSnapshot.getValue(Partida::class.java) // Obtener el valor del dataSnapshot
+            partida!!.id = dataSnapshot.key // Asignar el valor del campo "key" del dataSnapshot
             adaptadorPartidas.partidaCambio(partida)
         }
 
         override fun onChildRemoved(dataSnapshot: DataSnapshot) {
             Log.i(TAG, "onChildRemoved: ")
-            val partida = ??? // Obtener el valor del dataSnapshot
-            partida.id = ??? // Asignar el valor del campo "key" del dataSnapshot
+            val partida = dataSnapshot.getValue(Partida::class.java) // Obtener el valor del dataSnapshot
+            partida!!.id = dataSnapshot.key // Asignar el valor del campo "key" del dataSnapshot
             adaptadorPartidas.remover(partida)
         }
 
@@ -68,6 +64,25 @@ class ActividadPartidas : AppCompatActivity() {
             Log.i(TAG, "onCancelled: ")
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        //OK TODO-06-DATABASE
+        // Obtener una referencia a la base de datos, suscribirse a los cambios en Constantes.TABLA_PARTIDAS
+        // y agregar como ChildEventListener el listenerTablaPartidas definido mas abajo
+        FirebaseDatabase.getInstance()
+                .reference
+                .child(Constantes.TABLA_PARTIDAS)
+                .addChildEventListener(listenerTablaPartidas)
+
+    }
+
+    fun nuevaPartida() {
+        val intent = Intent(this, ActividadPartida::class.java)
+        startActivity(intent)
+    }
+/*
+
 
  */
 }
